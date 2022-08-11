1bfd16800e312bf86b51caa867f7728bb51077bf
lcmcstc <954440722@qq.com>
2022-08-11 13:21:00 +0800

    跨域

---
+++ src/main/java/com/example/demo/controller/filter/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller.filter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @description:跨域请求配置
+ * @author: GavenLee
+ * @time: 2020/10/20 9:55
+ */
+@Configuration
+public class CorsConfig  extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+
+}
+
---
+++ src/main/java/com/example/demo/controller/filter/CorsFilter.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @description:过滤器
+ * @author: GavenLee
+ * @time: 2020/10/20 9:53
+ */
+@Component
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest reqs = (HttpServletRequest) req;
+        String curOrigin = reqs.getHeader("Origin");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        chain.doFilter(req, res);
+    }
+
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        System.out.println("=======================================================出来了=================================================");
+    }
+
+    @Override
+    public void destroy() {}
+
+}
